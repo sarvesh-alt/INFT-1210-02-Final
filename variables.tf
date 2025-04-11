bea0c3c319424950d92d1c1a19520a303f799077
Sarvesh More <sarveshsudhir.more@dcmail.ca>
2025-04-11 15:39:42 -0400

    Update variables.tf

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "region" {
+  description = "AWS Region to deploy into"
+  type        = string
+  default     = "ca-central-1"
+}
+
+variable "availability_zone" {
+  description = "AWS Availability Zone"
+  type        = string
+  default     = "ca-central-1a"
+}
+
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+  default     = "615299769322"
+}
+
+variable "api_container_image" {
+  description = "Docker image URI in ECR for the final API"
+  type        = string
+  default     = ""  # Not required if using the ECR repository output
+}
+
+variable "ecs_execution_role_arn" {
+  description = "IAM role ARN for ECS task execution"
+  type        = string
+  default     = "arn:aws:iam::615299769322:role/ecsTaskExecutionRole"  # Update if your role name is different
+}
+
+# The ecs_task_role is now created by Terraform in main.tf.
