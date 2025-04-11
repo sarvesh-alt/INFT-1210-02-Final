b2db968d925a5ed4f6e9eb952f23e6f5fdffb129
Sarvesh More <sarveshsudhir.more@dcmail.ca>
2025-04-11 15:40:16 -0400

    commit

---
+++ outputs.tf
@@ -0,0 +1,19 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.more_vpc.id
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.more_alb.dns_name
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.more_ecs_cluster.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.more_ecs_service.name
+}
