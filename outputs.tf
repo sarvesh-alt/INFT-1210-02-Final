06cb9ad99495061e57038822229258fcd2b0fd33
Sarvesh More <sarveshsudhir.more@dcmail.ca>
2025-04-11 16:38:20 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = aws_vpc.more_final_vpc.id
+  value       = aws_vpc.morefinal_vpc.id
 }
 
 output "alb_dns_name" {
   description = "DNS name of the ALB"
-  value       = aws_lb.more_final_alb.dns_name
+  value       = aws_lb.morefinal_alb.dns_name
 }
 
 output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
-  value       = aws_ecs_cluster.more_final_ecs_cluster.name
+  value       = aws_ecs_cluster.morefinal_ecs_cluster.name
 }
 
 output "ecs_service_name" {
   description = "Name of the ECS service"
-  value       = aws_ecs_service.more_final_ecs_service.name
+  value       = aws_ecs_service.morefinal_ecs_service.name
 }
